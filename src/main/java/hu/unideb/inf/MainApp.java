ab57f7d506b30efd896aae49d9252901b1b514c3
Kovács Bence <bence1971387@gmail.com>
2021-04-19 16:36:42 +0200

    JavaFX, Hibernate template.

---
+++ src/main/java/hu/unideb/inf/MainApp.java
@@ -0,0 +1,74 @@
+package hu.unideb.inf;
+
+import hu.unideb.inf.model.Model;
+import hu.unideb.inf.view.FXMLStudentsSceneController;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import hu.unideb.inf.util.HibernateUtil;
+import hu.unideb.inf.entity.Student;
+
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/fxml/FXMLStudentsScene.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Students Register");
+        stage.setScene(scene);
+        
+        ((FXMLStudentsSceneController)loader.getController()).setModel(new Model());
+        
+        stage.show();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Student student = new Student("Ramesh", "Fadatare", "rameshfadatare@javaguides.com");
+        Student student1 = new Student("John", "Cena", "john@javaguides.com");
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student objects
+            session.save(student);
+            session.save(student1);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            List < Student > students = session.createQuery("from Student", Student.class).list();
+            students.forEach(s -> System.out.println(s.getFirstName()));
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        launch(args);
+    }
+
+}
---
+++ src/main/java/hu/unideb/inf/model/Student.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.unideb.inf.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author student
+ */
+public class Student {
+    private String name;
+    private int credits;
+    private LocalDate birthDate;
+
+    public Student(String name, int credits, LocalDate birthDate) {
+        this.name = name;
+        this.credits = credits;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+    
+}
