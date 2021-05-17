ddeace5aa937cba1aa3bbd474a262d4b036bb2a7
maciic <focimaci99@gmail.com>
2021-05-17 19:22:56 +0200

    A felesleg kitörlése

--- src/main/java/hu/unideb/inf/MainApp.java
+++ src/main/java/hu/unideb/inf/MainApp.java
@@ -1,20 +1,11 @@
 package hu.unideb.inf;
 
-import hu.unideb.inf.model.Model;
 import hu.unideb.inf.view.FXMLMainApp;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.util.List;
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
-import hu.unideb.inf.util.HibernateUtil;
-import hu.unideb.inf.entity.Student;
 
 
 public class MainApp extends Application {
@@ -25,49 +16,13 @@ public class MainApp extends Application {
         Scene scene = new Scene(loader.load());
         stage.setTitle("Students Register");
         stage.setScene(scene);
-        
-        ((FXMLMainApp)loader.getController()).setModel(new Model());
 
         stage.setResizable(false);          //Nem engedi hogy átméretezzék az ablakot
         stage.show();
     }
 
-    /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Student student = new Student("Ramesh", "Fadatare", "rameshfadatare@javaguides.com");
-        Student student1 = new Student("John", "Cena", "john@javaguides.com");
-        Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            // start a transaction
-            transaction = session.beginTransaction();
-            // save the student objects
-            session.save(student);
-            session.save(student1);
-            // commit transaction
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
 
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            List < Student > students = session.createQuery("from Student", Student.class).list();
-            students.forEach(s -> System.out.println(s.getFirstName()));
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
         launch(args);
     }
 
--- src/main/java/hu/unideb/inf/view/FXMLMainApp.java
+++ src/main/java/hu/unideb/inf/view/FXMLMainApp.java
@@ -5,42 +5,15 @@
  */
 package hu.unideb.inf.view;
 
-import hu.unideb.inf.model.Model;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-/**
- * FXML Controller class
- *
- * @author student
- */
+
 public class FXMLMainApp implements Initializable {
-    
-    private Model model;
 
-    public void setModel(Model model) {
-        this.model = model;
-    }
-        
-    @FXML
-    private Label colorLabel;
-    
-    @FXML
-    void handleButtonPushed() {
-        System.out.println("Button!!!!");
-        /*if (colorLabel.getText().equals("Black")) {
-            colorLabel.setText("White");
-        } else {
-            colorLabel.setText("Black");
-        }*/colorLabel.setText(model.getStudent().getName());
-    }
-
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
