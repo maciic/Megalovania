5f7e0de5d694fca503cec85e3790bccb6af85fd6
maciic <focimaci99@gmail.com>
2021-05-17 20:17:22 +0200

    FXMLMainApp.java kód hozzáadása

--- src/main/java/hu/unideb/inf/view/FXMLMainApp.java
+++ src/main/java/hu/unideb/inf/view/FXMLMainApp.java
@@ -7,16 +7,55 @@ package hu.unideb.inf.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 
 public class FXMLMainApp implements Initializable {
 
+    ObservableList<String> cb_orderByCostList = FXCollections.observableArrayList("---", "Növekvő", "Csökkenő");
+
+    @FXML private Label lb_itemsInBasket;
+    @FXML private ChoiceBox cb_orderByCostBox;
+    @FXML private ChoiceBox cb_orderByNameBox;
+
+    @FXML
+    private void ac_addToBasket() {
+        int szam = Integer.parseInt(lb_itemsInBasket.getText());
+        szam++;
+        lb_itemsInBasket.setText(String.valueOf(szam));
+    }
+
+    @FXML
+    private void ac_openBasket(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/FXMLUserOrder.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        cb_orderByCostBox.setValue("---");
+        cb_orderByCostBox.setItems(cb_orderByCostList);
+
+        cb_orderByNameBox.setValue("Növekvő");
+        cb_orderByNameBox.setItems(cb_orderByCostList);
     }
     
 }
