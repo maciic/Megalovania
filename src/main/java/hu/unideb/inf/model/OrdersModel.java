7fc58b2fadeb4622b5e216991b0c3909cedbdbbf
Kovács Bence <bence1971387@gmail.com>
2021-05-19 19:52:31 +0100

    orders model update

--- src/main/java/hu/unideb/inf/model/OrdersModel.java
+++ src/main/java/hu/unideb/inf/model/OrdersModel.java
@@ -3,6 +3,7 @@ package hu.unideb.inf.model;
 import hu.unideb.inf.util.Database;
 import hu.unideb.inf.util.Session;
 
+import java.sql.Array;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -117,5 +118,51 @@ public class OrdersModel {
 
 
         return true;
+    }
+    public ArrayList<HashMap<String,String>> getOrderList(){
+        ArrayList tmp = new ArrayList();
+        ArrayList<HashMap<String,String>> result = new ArrayList<>();
+        tmp.add(userId);
+        //SELECT * FROM user_orders INNER JOIN ordered_products ON user_orders.id = ordered_products.fk_order INNER JOIN invoices ON invoices.fk_order = user_orders.id INNER JOIN users ON user_orders.fk_user = users.id INNER JOIN products ON products.id = ordered_products.fk_product WHERE users.username = "tesztjános";
+        ResultSet rs = db.query(
+                "SELECT user_orders.id, user_orders.city_name, user_orders.postcode, user_orders.address, user_orders.house_num, user_orders.door_num, " +
+                "invoices.city_name, invoices.postcode, invoices.address, invoices.house_num, invoices.door_num, invoices.total, " +
+                "products.price, products.name, products.description, products.in_stock_qty FROM user_orders " +
+                "INNER JOIN ordered_products ON user_orders.id = ordered_products.fk_order " +
+                "INNER JOIN invoices ON invoices.fk_order = user_orders.id " +
+                "INNER JOIN users ON user_orders.fk_user = users.id " +
+                "INNER JOIN products ON products.id = ordered_products.fk_product " +
+                "WHERE users.id = ?;",tmp);
+        System.out.println(rs);
+        try {
+            while(rs != null && rs.next()){
+                HashMap<String,String> rstmp = new HashMap<>();
+
+                rstmp.put("order_id",rs.getString(1));
+
+                rstmp.put("order_city_name",rs.getString(2));
+                rstmp.put("order_postcode",rs.getString(3));
+                rstmp.put("order_address",rs.getString(4));
+                rstmp.put("order_house_num",rs.getString(5));
+                rstmp.put("order_door_num",rs.getString(6));
+
+                rstmp.put("invoices_city_name",rs.getString(7));
+                rstmp.put("invoices_postcode",rs.getString(8));
+                rstmp.put("invoices_address",rs.getString(9));
+                rstmp.put("invoices_house_num",rs.getString(10));
+                rstmp.put("invoices_door_num",rs.getString(11));
+                rstmp.put("invoices_total",rs.getString(12));
+
+                rstmp.put("product_price",rs.getString(13));
+                rstmp.put("product_name",rs.getString(14));
+                rstmp.put("product_description",rs.getString(15));
+                rstmp.put("product_in_stock_qty",rs.getString(16));
+
+                result.add(rstmp);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return result;
     }
 }
