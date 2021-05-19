1ae8b3960ea6d13f98840f99f3de6695217c0d4b
Szabo Dániel <danieltailor2@gmail.com>
2021-05-19 16:11:13 +0200

    Login feltételek megadása++.

--- src/main/java/hu/unideb/inf/view/FXMLUserLogin.java
+++ src/main/java/hu/unideb/inf/view/FXMLUserLogin.java
@@ -26,32 +26,34 @@ public class FXMLUserLogin implements Initializable {
         if((!txf_userName.getText().isEmpty()) && (!txf_userPassword.getText().isEmpty())){
             if(UsersModel.getUser(txf_userName.getText())!=null)
                 if(UsersModel.getUser(txf_userName.getText()).get(2).equals(txf_userPassword.getText())){
+                    label_WrongPassword.setText("");
+                    String username = txf_userName.getText();
+                    String password = txf_userPassword.getText();
 
-
-
-
-            String username = txf_userName.getText();
-            String password = txf_userPassword.getText();
-
-            try {
-                Stage currentStage = (Stage) txf_userName.getScene().getWindow();
-                currentStage.close();
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/FXMLMainApp.fxml"));
-                Parent root1 = (Parent) fxmlLoader.load();
-                Stage stage = new Stage();
-                stage.setScene(new Scene(root1));
-                stage.show();
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-            }else label_WrongPassword.setText("Helytelen jelszó!");
-        } else {
-            if(txf_userName.getText().isEmpty()){
+                    try {
+                        Stage currentStage = (Stage) txf_userName.getScene().getWindow();
+                        currentStage.close();
+                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/FXMLMainApp.fxml"));
+                        Parent root1 = (Parent) fxmlLoader.load();
+                        Stage stage = new Stage();
+                        stage.setScene(new Scene(root1));
+                        stage.show();
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+            }else
+                label_WrongPassword.setText("Helytelen jelszó!");
+                else label_WrongPassword.setText("Nincs ilyen felhasználó!");
+        } else
+            {
+                if(txf_userName.getText().isEmpty()){
                 txf_userName.setPromptText("Add meg a felasználó neved!");
+                label_WrongPassword.setText("");
             }
             if(txf_userPassword.getText().isEmpty()){
                 txf_userPassword.setPromptText("Add meg a jelszavad!");
+                label_WrongPassword.setText("");
             }
 
         }
