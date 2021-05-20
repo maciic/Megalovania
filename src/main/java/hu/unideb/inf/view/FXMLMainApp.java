327ff4871fd618046e78cbaa07ff481d9d2fd4d0
kerimartin <keri.martin59@gmail.com>
2021-05-20 14:25:39 +0300

    Működő kereső és rendező a főoldalon.

--- src/main/java/hu/unideb/inf/view/FXMLMainApp.java
+++ src/main/java/hu/unideb/inf/view/FXMLMainApp.java
@@ -6,9 +6,12 @@
 package hu.unideb.inf.view;
 
 
+import java.beans.EventHandler;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.ResourceBundle;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -33,20 +36,27 @@ import hu.unideb.inf.view.FXMLUserOrder;
 public class FXMLMainApp implements Initializable {
 
 
-    @FXML private Label lb_itemsInBasket;
-    @FXML private ChoiceBox cb_orderByCostBox;
-    @FXML private ChoiceBox cb_orderByNameBox;
-    @FXML private TextField txf_searchField;
-    @FXML private Button bt_loginName;
-
-    @FXML private AnchorPane prod_list;
+    @FXML
+    private Label lb_itemsInBasket;
+    @FXML
+    private ChoiceBox cb_orderByCostBox;
+    @FXML
+    private ChoiceBox cb_orderByNameBox;
+    @FXML
+    private TextField txf_searchField;
+    @FXML
+    private Button bt_loginName;
+    @FXML
+    private Button bt_sort;
+    @FXML
+    private AnchorPane prod_list;
 
     @FXML
     public void ac_addToBasket(String gombId) {
-        HashMap<String,String> tmp = new HashMap<>(ProductsModel.getProduct(gombId));
+        HashMap<String, String> tmp = new HashMap<>(ProductsModel.getProduct(gombId));
         Cart.AddToCart(tmp);
 
-        if(Cart.cartContent.size()!=0)
+        if (Cart.cartContent.size() != 0)
             lb_itemsInBasket.setText(String.valueOf(Cart.cartContent.size()));
         else
             lb_itemsInBasket.setText("0");
@@ -55,7 +65,7 @@ public class FXMLMainApp implements Initializable {
     }
 
     @FXML
-    private void ac_openBasket(){
+    private void ac_openBasket() {
 
         try {
             lb_itemsInBasket.setText(String.valueOf(Cart.cartContent.size()));
@@ -68,14 +78,13 @@ public class FXMLMainApp implements Initializable {
                 // Save file
             });
             stage.show();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @FXML
-    private void ac_logout(){
+    private void ac_logout() {
         try {
             Stage currentStage = (Stage) lb_itemsInBasket.getScene().getWindow();
             currentStage.close();
@@ -84,89 +93,116 @@ public class FXMLMainApp implements Initializable {
             Stage stage = new Stage();
             stage.setScene(new Scene(root1));
             stage.show();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @FXML
-    private void ac_listUserOrders(){
+    private void ac_listUserOrders() {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/FXMLlistUserOrders.fxml"));
             Parent root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
             stage.setScene(new Scene(root1));
             stage.show();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @FXML
-    private void ac_search(){
+    private void ac_search() {
         String search;
-        if(txf_searchField.getText() != ""){
-            search = txf_searchField.getText();
-            System.out.println(search);
-        }
+        search = txf_searchField.getText();
+        System.out.println(search);
+        getSearch(search);
     }
+
     @FXML
-    private void orderRefresh(){
-        prod_list.getChildren().removeAll();
-        lb_itemsInBasket.setText(String.valueOf(Cart.cartContent.size()));
-        ProductsModel.OrderByName temp1;
+    private void getChoice(ChoiceBox cb_orderByNameBox, ChoiceBox cb_orderByCostBox) {
+        ProductsModel.OrderByPrice PriceOrder = ProductsModel.OrderByPrice.None;
+        ProductsModel.OrderByName NameOrder = ProductsModel.OrderByName.None;
 
-        if(cb_orderByNameBox.getValue().equals("---")){
-            temp1 = ProductsModel.OrderByName.None;
-        }else if(cb_orderByNameBox.getValue().equals("Növekvő")) {
-            temp1 = ProductsModel.OrderByName.Ascending;
-        } else
-            temp1 = ProductsModel.OrderByName.Descending;
+        if (cb_orderByCostBox.getValue().equals("Csökkenő")) {
+            PriceOrder = ProductsModel.OrderByPrice.Descending;
+        } else if (cb_orderByCostBox.getValue().equals("Növekvő")) {
+            PriceOrder = ProductsModel.OrderByPrice.Ascending;
+        } else {
+            PriceOrder = ProductsModel.OrderByPrice.None;
+        }
 
-        ProductsModel.OrderByPrice temp2;
-
-        if(cb_orderByCostBox.getValue().equals("---")){
-            temp2 = ProductsModel.OrderByPrice.None;
-        }else if(cb_orderByCostBox.getValue().equals("Növekvő")) {
-            temp2 = ProductsModel.OrderByPrice.Ascending;
-        } else
-            temp2 = ProductsModel.OrderByPrice.Descending;
+        if (cb_orderByNameBox.getValue().equals("Csökkenő")) {
+            NameOrder = ProductsModel.OrderByName.Descending;
+        } else if (cb_orderByNameBox.getValue().equals("Növekvő")) {
+            NameOrder = ProductsModel.OrderByName.Ascending;
+        } else {
+            NameOrder = ProductsModel.OrderByName.None;
+        }
 
         int x = 0;
         int y = 0;
-        int i=0;
+        int i = 0;
 
-        for (var elem : ProductsModel.getProducts(temp1,temp2))
-        {
-            if(i*50*i > prod_list.getPrefHeight())
-            {
-                prod_list.setPrefHeight((i)*50+(i));
+        prod_list.getChildren().removeAll();
+
+        for (var elem : ProductsModel.getProducts(NameOrder, PriceOrder)) {
+
+
+            if (i * 50 * i > prod_list.getPrefHeight()) {
+                prod_list.setPrefHeight((i) * 50 + (i));
             }
 
-            if(x < 5)
-            {
-                prod_list.getChildren().add(createPane(x*220, y * 230,elem.get("name"),Integer.parseInt(elem.get("id")),Integer.parseInt(elem.get("price")),elem.get("id")));
-                x++;
+            if (elem.get("name").toLowerCase().contains(txf_searchField.getText().toLowerCase())) {
+                if (x < 5) {
+                    prod_list.getChildren().add(createPane(x * 220, y * 230, elem.get("name"), Integer.parseInt(elem.get("id")), Integer.parseInt(elem.get("price")), elem.get("id")));
+                    x++;
+                } else {
+                    y++;
+                    x = 0;
+                    prod_list.getChildren().add(createPane(x * 220, y * 230, elem.get("name"), Integer.parseInt(elem.get("id")), Integer.parseInt(elem.get("price")), elem.get("id")));
+                }
+                i++;
             }
-            else
-            {
-                y++;
-                x = 0;
-                prod_list.getChildren().add(createPane(x*220, y * 230,elem.get("name"),Integer.parseInt(elem.get("id")),Integer.parseInt(elem.get("price")),elem.get("id")));
+        }
+    }
+
+    @FXML
+    private void getSearch(String sub) {
+
+        int x = 0;
+        int y = 0;
+        int i = 0;
+
+        prod_list.getChildren().clear();
+
+        for (var elem : ProductsModel.getProducts(ProductsModel.OrderByName.None, ProductsModel.OrderByPrice.None)) {
+
+
+            if (i * 50 * i > prod_list.getPrefHeight()) {
+                prod_list.setPrefHeight((i) * 50 + (i));
             }
-            i++;
+
+            if (elem.get("name").toLowerCase().contains(sub.toLowerCase())) {
+                if (x < 5) {
+                    prod_list.getChildren().add(createPane(x * 220, y * 230, elem.get("name"), Integer.parseInt(elem.get("id")), Integer.parseInt(elem.get("price")), elem.get("id")));
+                    x++;
+                } else {
+                    y++;
+                    x = 0;
+                    prod_list.getChildren().add(createPane(x * 220, y * 230, elem.get("name"), Integer.parseInt(elem.get("id")), Integer.parseInt(elem.get("price")), elem.get("id")));
+                }
+                i++;
+            }
         }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
         cb_orderByNameBox.getItems().add("---");
         cb_orderByNameBox.getItems().add("Növekvő");
         cb_orderByNameBox.getItems().add("Csökkenő");
-        cb_orderByNameBox.setValue("Növekvő");
+        cb_orderByNameBox.setValue("---");
 
         cb_orderByCostBox.getItems().add("---");
         cb_orderByCostBox.getItems().add("Növekvő");
@@ -175,21 +211,23 @@ public class FXMLMainApp implements Initializable {
 
         ProductsModel.OrderByName temp1;
 
-        if(cb_orderByNameBox.getValue().equals("---")){
+        if (cb_orderByNameBox.getValue().equals("---")) {
             temp1 = ProductsModel.OrderByName.None;
-        }else if(cb_orderByNameBox.getValue().equals("Növekvő")) {
+        } else if (cb_orderByNameBox.getValue().equals("Növekvő")) {
             temp1 = ProductsModel.OrderByName.Ascending;
         } else
             temp1 = ProductsModel.OrderByName.Descending;
 
         ProductsModel.OrderByPrice temp2;
 
-        if(cb_orderByCostBox.getValue().equals("---")){
+        if (cb_orderByCostBox.getValue().equals("---")) {
             temp2 = ProductsModel.OrderByPrice.None;
-        }else if(cb_orderByCostBox.getValue().equals("Növekvő")) {
+        } else if (cb_orderByCostBox.getValue().equals("Növekvő")) {
             temp2 = ProductsModel.OrderByPrice.Ascending;
         } else
             temp2 = ProductsModel.OrderByPrice.Descending;
+
+        bt_sort.setOnAction(actionEvent -> getChoice(cb_orderByNameBox, cb_orderByCostBox));
 
         bt_loginName.setText("Felhasználó");
 
@@ -197,31 +235,26 @@ public class FXMLMainApp implements Initializable {
         int y = 0;
 
 
-        int i=0;
+        int i = 0;
 
-        for (var elem : ProductsModel.getProducts(temp1,temp2))
-        {
-            if(i*50*i > prod_list.getPrefHeight())
-            {
-                prod_list.setPrefHeight((i)*50+(i));
+        for (var elem : ProductsModel.getProducts(temp1, temp2)) {
+            if (i * 50 * i > prod_list.getPrefHeight()) {
+                prod_list.setPrefHeight((i) * 50 + (i));
             }
 
-            if(x < 5)
-            {
-                prod_list.getChildren().add(createPane(x*220, y * 230,elem.get("name"),Integer.parseInt(elem.get("id")),Integer.parseInt(elem.get("price")),elem.get("id")));
+            if (x < 5) {
+                prod_list.getChildren().add(createPane(x * 220, y * 230, elem.get("name"), Integer.parseInt(elem.get("id")), Integer.parseInt(elem.get("price")), elem.get("id")));
                 x++;
-            }
-            else
-            {
+            } else {
                 y++;
                 x = 0;
-                prod_list.getChildren().add(createPane(x*220, y * 230,elem.get("name"),Integer.parseInt(elem.get("id")),Integer.parseInt(elem.get("price")),elem.get("id")));
+                prod_list.getChildren().add(createPane(x * 220, y * 230, elem.get("name"), Integer.parseInt(elem.get("id")), Integer.parseInt(elem.get("price")), elem.get("id")));
             }
-        i++;
+            i++;
         }
     }
 
-    private AnchorPane createPane(int xpadd, int ypadd, String name, int id, int cost,String buttonID){
+    private AnchorPane createPane(int xpadd, int ypadd, String name, int id, int cost, String buttonID) {
 
         AnchorPane ap_item = new AnchorPane();
         Label lb_name = new Label(name);
@@ -238,12 +271,12 @@ public class FXMLMainApp implements Initializable {
         bt_buyitem.setAlignment(Pos.CENTER);
         bt_buyitem.setOnAction(actionEvent -> ac_addToBasket(tovabb));
 
-        lb_name.setFont(javafx.scene.text.Font.font ("System", 20));
+        lb_name.setFont(javafx.scene.text.Font.font("System", 20));
         lb_name.setLayoutX(55);
         lb_name.setLayoutY(40);
         lb_name.setAlignment(Pos.CENTER);
 
-        lb_cost.setFont(Font.font ("System", 20));
+        lb_cost.setFont(Font.font("System", 20));
         lb_cost.setLayoutX(74);
         lb_cost.setLayoutY(80);
         lb_name.setAlignment(Pos.CENTER);
@@ -258,8 +291,6 @@ public class FXMLMainApp implements Initializable {
         ap_item.getChildren().add(bt_buyitem);
         ap_item.getChildren().add(lb_cost);
 
-
         return ap_item;
     }
-
 }
